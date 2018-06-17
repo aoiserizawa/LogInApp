af4ecd788a119c4b7f91ae9e85dbb547406ebcf0
alvin joseph valdez <alvinjosephvaldez@Macintoshs-MacBook-Pro-3.local>
2018-06-18 01:36:22 +0800

    in-progress

--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import '../models/Todo.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
 
 class Home extends StatefulWidget {
   final List<Todo> todos;
@@ -15,23 +19,53 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home>{
   final List<Todo> todos;
+  List<String> myTitles = new List();
 
   HomeState({Key key, @required this.todos});
 
   @override
+  void initState() {
+    super.initState();
+
+    var titke = this.getJson().then((List value){
+      value.forEach((element){
+        setState(() {
+          myTitles.add(element["title"].toString());
+          print(myTitles);
+        });
+
+      });
+    });
+
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+
+
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Home Screen"),
       ),
       body: new ListView.builder(
-          itemCount: this.todos.length,
+          itemCount: myTitles.length -1,
           itemBuilder: (context, index){
             return new ListTile(
-              title: new Text(todos[index].title),
+              title: new Text(myTitles[index]),
             );
           })
     );
   }
 
+
+  Future<List<dynamic>> getJson() async {
+    String apiURL = "https://jsonplaceholder.typicode.com/posts";
+
+    final response = await http.get(apiURL);
+
+    return json.decode(response.body);
+
+  }
 }
